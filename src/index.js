19da106a8b5c3351708ac0143c6d95d3d994c5f8
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-23 01:21:18 -0400

    fixed favicon link ref

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/styles/style.css";
-import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
