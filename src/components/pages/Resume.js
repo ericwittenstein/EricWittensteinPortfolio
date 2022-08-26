890a616d1aeade8e45351d6b6dc0cf3f7ca6a729
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-26 13:03:09 -0400

    giving up on that aspect of the PDF viewer, gonna do it a different way, cleaned up files

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,29 +1,13 @@
 import React from "react";
-// import { useState } from "react";
-// import { Document, Page } from "react-pdf";
-// import SinglePagePDFViewer from "../pdf/single-page";
-// import AllPagesPDFViewer from "../pdf/all-pages";
-// import { pdfjs } from "react-pdf";
-import PdfViewerComponent from "../PdfViewerComponent";
 
 import EricWittensteinResume from "../images/EricWittensteinResume.pdf";
 
 export default function Resume() {
-	// const [EricWittensteinResume, setDocument] = useState(
-	// 	"../images/EricWittensteinResume.pdf"
-	// );
-
-	// const handleOpen = () => setDocument("../images/emailIcon.png");
-
 	return (
 		<div className="main">
 			<h3>Resume</h3>
-			{/* <SinglePagePDFViewer pdf={EricWittensteinResume} /> */}
-			{/* <AllPagesPDFViewer pdf={EricWittensteinResume} /> */}
 			<div className="PDF-window">
-				<div className="PDF-viewer">
-					<PdfViewerComponent document={EricWittensteinResume} />
-				</div>
+				<div className="PDF-viewer"></div>
 			</div>
 		</div>
 	);
