64db09bec7778d8d59b3333fb9dd49e01525f746
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-26 04:07:17 -0400

    still working on Resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { Document, Page } from "react-pdf";
+// import { useState } from "react";
+// import { Document, Page } from "react-pdf";
+// import SinglePagePDFViewer from "../pdf/single-page";
+// import AllPagesPDFViewer from "../pdf/all-pages";
+// import { pdfjs } from "react-pdf";
+import PdfViewerComponent from "../PdfViewerComponent";
 
-import EricWittensteinResume from "../images/Eric Wittenstein Resume.pdf";
+import EricWittensteinResume from "../images/EricWittensteinResume.pdf";
 
 export default function Resume() {
+	// const [EricWittensteinResume, setDocument] = useState(
+	// 	"../images/EricWittensteinResume.pdf"
+	// );
+
+	// const handleOpen = () => setDocument("../images/emailIcon.png");
+
 	return (
 		<div className="main">
 			<h3>Resume</h3>
-			<Document file={EricWittensteinResume}>
-				<Page pageNumber={1} />
-			</Document>
+			{/* <SinglePagePDFViewer pdf={EricWittensteinResume} /> */}
+			{/* <AllPagesPDFViewer pdf={EricWittensteinResume} /> */}
+			<div className="PDF-window">
+				<div className="PDF-viewer">
+					<PdfViewerComponent document={EricWittensteinResume} />
+				</div>
+			</div>
 		</div>
 	);
 }
