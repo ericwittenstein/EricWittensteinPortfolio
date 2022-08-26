ff0babeeaacb1bafbbeb595893477d2c48b26c84
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-26 01:52:30 -0400

    about to add new npm package to test pdf viewer

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
 			<h3>About Me</h3>
 			<p>
 			Hi there, and welcome to my portfolio page. My name is Eric Wittenstein, and I'm looking to begin my career
-            as a web developer. I'm currently enrolled in a "Coding Bootcamp" through Georgia Tech, finishing in early
+            as a web developer. I'm currently enrolled in a Coding Bootcamp through Georgia Tech, finishing in early
             September, and will be using this webpage to showcase my completed projects and demonstrate my skills as a
             developer.
 			</p>
