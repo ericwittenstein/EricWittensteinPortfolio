911ec49ebd21b5c8b9bc4bc270ac05947c00f358
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-25 11:19:07 -0500

    added bl3t tracker and screenshots

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,11 +1,50 @@
 import React from "react";
 import sitSS from "../assets/stayintouch.png";
 import invSS from "../assets/inventory.png";
+import bl3tss from "../assets/bl3t.png";
 
 export default function Projects() {
 	return (
 		<div className="main">
 			<h3>Projects</h3>
+			<div className="project">
+				<img
+					src={bl3tss}
+					alt="Borderlands 3 Item Tracker"
+					className="project-bg"
+				/>
+				<div className="project-text">
+					<h4>Borderlands 3 Item Tracker (BL3T)</h4>{" "}
+					<a
+						href="https://github.com/ericwittenstein/Borderlands-3-Tracker"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						GitHub Repo
+					</a>{" "}
+					<a
+						href="https://bl3tracker.herokuapp.com/"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						Deployed App
+					</a>
+					<p>
+						<br />
+						Working independently on a personal project, created a
+						full stack data management application designed to enter
+						and track information about items found in the game
+						Borderlands 3, as well as whether they are useful or not
+						for various use cases as determined by character type,
+						build, etc.
+					</p>
+					<p>
+						Technologies used: HTML, CSS, JavaScript, Typescript,
+						Bootstrap, React, React Hooks, Axios, Node.js,
+						Express.js, MySQL, Sequelize, JawsDB, RestAPIs, Heroku
+					</p>
+				</div>
+			</div>
 			<div className="project">
 				<img
 					src={sitSS}
@@ -30,10 +69,16 @@ export default function Projects() {
 					</a>
 					<p>
 						<br />
-						Working with a team of 3 other developers, we created a functional social network application that can be used by graduates of our coding bootcamp to stay in contact with one another after graduation
+						Working with a team of 3 other developers, we created a
+						functional social network application that can be used
+						by graduates of our coding bootcamp to stay in contact
+						with one another after graduation
 					</p>
 					<p>
-						Technologies used: HTML, CSS, JavaScript, Tailwind, Bootstrap, Materialize, Google Fonts, React.js, Node.js, Express.js, GraphQL, MongoDB, Mongoose, JWT, Figma, Lucid, Heroku
+						Technologies used: HTML, CSS, JavaScript, Tailwind,
+						Bootstrap, Materialize, Google Fonts, React.js, Node.js,
+						Express.js, GraphQL, MongoDB, Mongoose, JWT, Figma,
+						Lucid, Heroku
 					</p>
 				</div>
 			</div>
